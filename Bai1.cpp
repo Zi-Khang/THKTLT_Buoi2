0f8ba82de800b9580c19c702c80f3fa27b8110e9
Vi Khang <trieuvikhang003@gmail.com>
2024-07-17 15:42:47 +0700

    Phan2_Bai1_4

--- Bai1.cpp
+++ Bai1.cpp
@@ -3,6 +3,7 @@
 #include <stdlib.h>
 #include <conio.h>
 #include <ctime>
+
 #define MAX 1000
 
 
@@ -11,31 +12,44 @@ void xuatMang(int arr[], int n);
 void nhapMangChan(int arr[], int &n);
 void xuatMangChan(int arr[], int n);
 int timX(int arr[], int &n, int &x);
+void Interchange_Sort_Tang(int arr[], int n);
+void Interchange_Sort_Giam(int arr[], int n);
 
 int main()
 {
 	//Bai1
 	int myArray[MAX]; 
 	int nSize;
-	nhapMang(myArray, nSize);
-	xuatMang(myArray, nSize);
+	//nhapMang(myArray, nSize);
+	//xuatMang(myArray, nSize);
+	////
+	////Bai2
+	//int myArray2[MAX];
+	//nhapMangChan(myArray2, nSize);
+	//xuatMangChan(myArray2, nSize);
 	//
-	//Bai2
-	int myArray2[MAX];
-	nhapMangChan(myArray2, nSize);
-	xuatMangChan(myArray2, nSize);
-	
-	//Bai3
-	int myArray3[MAX];
-	int x;
-	int check = timX(myArray3, nSize, x);
-	if (check == -1)
-	{
-		printf("Mang khong chua gia tri ban can tim");
-	}else
-	{
-		printf("Mang chua gia tri ban can tim tai vi tri thu: %d", check + 1);
-	};
+	////Bai3
+	//int myArray3[MAX];
+	//int x;
+	//int check = timX(myArray3, nSize, x);
+	//if (check == -1)
+	//{
+	//	printf("Mang khong chua gia tri ban can tim");
+	//}else
+	//{
+	//	printf("Mang chua gia tri ban can tim tai vi tri thu: %d", check + 1);
+	//};
+
+	//Bai4
+	int myArray4[MAX];
+	nhapMang(myArray4, nSize);
+	printf("Sap xep tang: \n");
+	Interchange_Sort_Tang(myArray4, nSize);
+	xuatMang(myArray4, nSize);
+	printf("Sap xep giam: \n");
+	Interchange_Sort_Giam(myArray4, nSize);
+	xuatMang(myArray4, nSize);
+
 	getch();
 }
 
@@ -113,3 +127,29 @@ int timX(int arr[], int &n, int &x)
 	}
 	return -1;
 }
+
+void Interchange_Sort_Tang(int arr[], int n){
+	int i, j, x;
+	for (i = 0; i<n - 1; i++){
+		for (j = i + 1; j<n; j++){
+			if (arr[i]>arr[j]){
+				x = arr[i];
+				arr[i] = arr[j];
+				arr[j] = x;;
+			}
+		}
+	}
+}
+void Interchange_Sort_Giam(int arr[], int n){
+	int i, j, x;
+	for (i = 0; i<n - 1; i++){
+		for (j = i + 1; j<n; j++){
+			if (arr[i]<arr[j]){
+				x = arr[i];
+				arr[i] = arr[j];
+				arr[j] = x;;
+			}
+		}
+	}
+}
+
