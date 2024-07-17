3f02abe57967251c5ae8442496d86fee7ee3cdfe
Vi Khang <trieuvikhang003@gmail.com>
2024-07-17 14:17:39 +0700

    Phan2_Bai1_1

---
+++ Bai1.cpp
@@ -0,0 +1,46 @@
+﻿#include <iostream>
+#include <cstdlib>
+#include <stdlib.h>
+#include <conio.h>
+#include <ctime>
+#define MAX 1000
+
+
+void nhapMang(int arr[], int &n);
+void xuatMang(int arr[], int n);
+
+int main()
+{
+	int myArray[MAX]; 
+	int nSize;
+
+	nhapMang(myArray, nSize);
+	xuatMang(myArray, nSize);
+
+	getch();
+}
+
+void nhapMang(int arr[], int &n)
+{
+	srand((unsigned int)time(NULL));;
+	printf("Hay nhap n: ");
+	scanf("%d", &n);
+
+	if (n > 0)
+	{
+		for (int i = 0; i < n; i++)
+		{
+			arr[i] = 15 + rand();
+		}
+	}
+}
+
+// hàm xuất mảng
+void xuatMang(int arr[], int n)
+{
+	// xuất từng phần tử cho mảng từ chỉ số 0 đến n – 1
+	for (int i = 0; i < n; i++)
+	{
+		printf("a[%d] = %d\n", i, arr[i]);
+	}
+}
